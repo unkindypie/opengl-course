c0f376b9f84bfa0be8627863f996f5b43d7e4751
Max Bondarenko <maxxbondd@gmail.com>
2020-03-09 23:46:16 +0200

    3d piramid

--- main.cpp
+++ main.cpp
@@ -2,22 +2,33 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <iostream>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/string_cast.hpp>
 
 // Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
-GLuint VAO, VBO, shader, unifromModel;
+const float toRadians = M_PI / 180;
+GLuint VAO, VBO, IBO, shader, unifromModel;
 
 bool direction = true;
 float triOffset = 0.0f;
-float triMaxOffset = 0.7f;
-float triIncement = 0.005f;
+float triMaxOffset = 1.f;
+float triIncement = 0.01f;
+
+bool sizeDirection = true;
+float curSize = 0.4f;
+float maxSize = 0.8f;
+float minSize = 0.1f;
+
+float curAngle = .0f;
 
 //Vertex shader
 static const char* vShader =
@@ -25,27 +36,46 @@ static const char* vShader =
     #version 330\n\
     layout (location = 0) in vec3 pos;\n\
     uniform mat4 model;\n\
+    out vec4 vCol;//после вызова этого шейдера эта переменная будет передана в следующий шейдер\n\
     void main() {\n\
-        gl_Position = model * vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);\n\
+        gl_Position = model * vec4(pos, 1.0);\n\
+        vCol = vec4(clamp(pos, 0.0, 1.0), 1);\n\
     }";
 //Fragment shader
 static const char* fShader =
     "\n\
     #version 330\n\
     out vec4 color;\n\
+    in vec4 vCol;\n\
     void main() {\n\
-        color = vec4(1.0, 0.5, 0.0, 1);\n\
+        color = vCol;//vec4(1.0, 0.5, 0.0, 1);\n\
     }";
 
 void createTriagnle(){
+    //индесксы для буфера индексов нужны для того, чтобы не
+    //копипастить вершины по нескольку раз, т.к. они часто повторяются в 3д фигурах
+    //по сути они ссылаются на вершины
+    unsigned int indeces[] = {
+      0, 3, 1, //1 сторона
+      1, 3, 2, //2 сторона
+      2, 3, 0, //3 сторона
+      0, 1, 2 //дно (прямо как я)
+    };
+
     GLfloat vertices[] = {
         -1.0f,  -1.0f, 0.0f,
+         0, -1, 1,
          1.0f, -1.0f, 0.0f,
          0.0f, 1.0f, 0.0f
     };
     //генерирует айдишники для массива вертексов
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
+        glGenBuffers(1, &IBO);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof (indeces), indeces, GL_STATIC_DRAW);
+
+
         //создает буфер для этого массива вертексов в памяти видюхи. Биндится он к самому последнему созданному VAO
         glGenBuffers(1, &VBO);
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -55,7 +85,7 @@ void createTriagnle(){
             glEnableVertexAttribArray(0); //включаю его
 
         glBindBuffer(GL_ARRAY_BUFFER, 0);
-
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     glBindVertexArray(0); //0 - дальнейшие операции с вертксными буферами уже не будут биндится к этому буферу
 
 }
@@ -119,6 +149,28 @@ void compileShaders(){
     unifromModel = glGetUniformLocation(shader, "model");
 }
 
+void printMat(glm::mat4 matrix) {
+    std::string str = glm::to_string(matrix);
+    size_t pos;
+    int skips = 0;
+    do {
+        pos = str.find_first_of(',');
+
+        if(pos > str.size()) break;
+
+        if(skips == 3) {
+            str.replace(pos, 1, "\n");
+            skips = 0;
+        }
+        else {
+            str.replace(pos, 1, " ");
+            skips++;
+        }
+    } while(pos);
+
+    std::cout << str << std::endl;
+}
+
 int main(int argc, char *argv[])
 {
 
@@ -165,12 +217,24 @@ int main(int argc, char *argv[])
         glfwTerminate();
         return 1;
     }
+    //включение проверки глубины(чтобы 3д вершины рисовались в правильном порядке)
+    glEnable(GL_DEPTH_TEST);
 
     // Setup Viewport size
     glViewport(0, 0, bufferWidth, bufferHeight);
 
     createTriagnle();
     compileShaders();
+
+    glm::mat4 _model(1); //конструктор еденичной матрицы
+    //_model = glm::translate(_model, glm::vec3(triOffset, 0.0f, 0.0f));
+    _model = glm::rotate(_model, 89 * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
+    printMat(_model);
+
+    glm::mat4 __model(1); //конструктор еденичной матрицы
+    __model = glm::rotate(__model, 89 * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
+     __model = glm::translate(__model, glm::vec3(2, 5.5f, 0.0f));
+    printMat(__model);
 
     // Loop until window closed
     while (!glfwWindowShouldClose(mainWindow))
@@ -186,25 +250,50 @@ int main(int argc, char *argv[])
         }
         if(std::abs(triOffset) >= triMaxOffset){
             direction = !direction;
+            glm::mat4 model(1); //конструктор еденичной матрицы
+            model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+            model = glm::scale(model, glm::vec3(curSize, curSize, 1.0f));
+            printMat(model);
+        }
+        curAngle += 0.5f;
+        if(curAngle >= 360){
+            curAngle = 0;
+        }
+
+        if(sizeDirection){
+            curSize += 0.001;
+        }
+        else {
+            curSize -= 0.001;
+        }
+        if(curSize > maxSize || curSize < minSize){
+            sizeDirection = !sizeDirection;
         }
 
         // Clear window
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         //говорю, к какому шейдеру сейчас отправлять вызовы всех шейдерных функций
         glUseProgram(shader);
 
         //создаю матрицу преобразований
         glm::mat4 model(1); //конструктор еденичной матрицы
-        model = glm::translate(model, glm::vec3(triOffset, triOffset, 0.0f));
+        model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+        //model = glm::scale(model, glm::vec3(curSize, curSize, 1.0f));
+        model = glm::rotate(model, curAngle * toRadians, glm::vec3(1.0f, 1.0f, 1.0f));
+        model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
 
-//        glUniform1f(uniformXMove, triOffset); //задаю юниформ шейдера
+
+
         //задаю юниформ матрицы преобразований, в который отправляю указатель на матрицу преобразований
         glUniformMatrix4fv(unifromModel, 1, GL_FALSE, glm::value_ptr(model));
 
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+        glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
         glBindVertexArray(0);
 
         glUseProgram(0);//отключаю шейдер
